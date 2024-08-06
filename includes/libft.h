a46432d67249f24d43f8a29f833e7f671bcc41b8
yhames <wjddn494@naver.com>
2024-08-06 18:41:25 +0900

    Add ft_isspace

--- includes/libft.h
+++ includes/libft.h
@@ -21,6 +21,7 @@ int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isnumeric(char *str);
+int		ft_isascii(int c);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memmove(void *dst, const void *src, size_t len);

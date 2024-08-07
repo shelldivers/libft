4b506b70d0b5989e4c9831c23a732d9afdf24457
yhames <wjddn494@naver.com>
2024-08-07 18:08:02 +0900

    Add ft_atof

--- includes/libft.h
+++ includes/libft.h
@@ -16,14 +16,14 @@
 # include "ft_type.h"
 # include <stdlib.h>
 
-int		ft_atof(const char *str);
+float	ft_atof(const char *str);
 int		ft_atoi(const char *str);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isnumeric(char *str);
 int		ft_isascii(int c);
-int     ft_isspace(char c);
+int		ft_isspace(char c);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memmove(void *dst, const void *src, size_t len);
--- srcs/ft_atof.c
+++ srcs/ft_atof.c
@@ -12,48 +12,41 @@
 
 #include "libft.h"
 
-static float	ft_pow(int n);
+static float	ft_powf(int n);
+static float	get_n_flag(char c);
 
-int	ft_atof(const char *str)
+float	ft_atof(const char *str)
 {
 	float	ac;
 	float	n_flag;
 	int		decimal;
 
-	// 공백 문자를 건너뛴다.
+	ac = 0.0;
 	while (ft_isspace(*str))
 		str++;
-	if (*str == '+')
-		str++;
-
-	// 부호를 확인한다.
-	n_flag = 1.0;
-	else if (*str == '-')
-	{
-		n_flag = -1.0;
-		str++;
-	}
-
-	// 정수 부분을 계산한다.
+	n_flag = get_n_flag(*str);
 	while (ft_isdigit(*str))
 		ac = ac * 10.0 + (*str++ - '0') * n_flag;
-
-	// 소수점을 건너뛴다.
 	if (*str == '.')
 		str++;
-
-	// 소수 부분을 계산한다.
 	decimal = 0;
 	while (ft_isdigit(*str))
 	{
-		ac = ac + (*str++ - '0') * n_flag;
+		ac = ac * 10.0 + (*str++ - '0') * n_flag;
 		decimal++;
 	}
-	ac /= ft_pow(decimal);
+	ac /= ft_powf(decimal);
 	return (ac);
 }
 
-static float	ft_pow(int n)
+float	get_n_flag(char c)
+{
+	if (c == '-')
+		return (-1.0);
+	return (1.0);
+}
+
+static float	ft_powf(int n)
 {
 	float	ret;
 

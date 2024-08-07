e7a0bb0ba6093e9d717ea6a09eec6be3bfff0fae
yhames <wjddn494@naver.com>
2024-08-07 18:46:48 +0900

    fix: negative

--- srcs/ft_atof.c
+++ srcs/ft_atof.c
@@ -24,7 +24,7 @@ float	ft_atof(const char *str)
 	ac = 0.0;
 	while (ft_isspace(*str))
 		str++;
-	n_flag = get_n_flag(*str);
+	n_flag = get_n_flag(*str++);
 	while (ft_isdigit(*str))
 		ac = ac * 10.0 + (*str++ - '0') * n_flag;
 	if (*str == '.')
